149843c48caacb0fc6c7db0ec0df7c6b169f1dd7
Johannes Alexis Wirde <johannes.wirde@gmail.com>
2024-03-21 12:53:56 +0100

    Before cli:.kts

---
+++ raspberryjam/api/databaseBeforeWrite.copyEmailOfUserToEmailTable.basic.raspberryjam.kts
@@ -0,0 +1,9 @@
+
+
+
+fun sum(one: Int, two: Int): Int {
+    return one + two
+}
+
+print(sum(1, 2))
+
---
+++ settings.gradle.kts
@@ -0,0 +1,30 @@
+
+
+rootProject.name="raspberryjam-app"
+
+pluginManagement {
+    plugins {
+        val kotlinVersion: String by settings
+        val shadowVersion: String by settings
+        val graalvmBuildToolsNativeVersion: String by settings
+
+        id("org.jetbrains.kotlin") version kotlinVersion
+        id("org.jetbrains.kotlin.multiplatform") version kotlinVersion
+        id("org.jetbrains.kotlin.plugin.serialization") version kotlinVersion
+        id("com.github.johnrengelman.shadow") version shadowVersion
+        id("org.graalvm.buildtools.native") version graalvmBuildToolsNativeVersion
+        //id("application") //TODO version
+    }
+
+    repositories {
+        gradlePluginPortal()
+        google()
+        mavenCentral()
+    }
+}
+
+include(":shared")
+include(":cli")
+
+project(":shared").name = "shared"
+project(":cli").name = "cli"
