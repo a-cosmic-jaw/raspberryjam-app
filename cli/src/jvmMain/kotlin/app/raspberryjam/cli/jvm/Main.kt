dc606b908fa337a9de08defc54616ef805436d79
Johannes Alexis Wirde <johannes.wirde@gmail.com>
2024-03-24 14:21:33 +0100

    Moved JvmMonitor

--- cli/src/jvmMain/kotlin/app/raspberryjam/cli/jvm/Main.kt
+++ cli/src/jvmMain/kotlin/app/raspberryjam/cli/jvm/Main.kt
@@ -1,7 +1,7 @@
 package app.raspberryjam.cli.jvm
 
 import app.raspberryjam.cli.command.Environment
-import app.raspberryjam.cli.command.JvmMonitor
+import app.raspberryjam.cli.jvm.command.JvmMonitor
 import app.raspberryjam.cli.command.Main
 import app.raspberryjam.cli.command.Project
 import com.github.ajalt.clikt.core.subcommands
--- cli/src/jvmMain/kotlin/app/raspberryjam/cli/jvm/command/Monitor.kt
+++ cli/src/jvmMain/kotlin/app/raspberryjam/cli/jvm/command/Monitor.kt
@@ -1,5 +1,6 @@
-package app.raspberryjam.cli.command
+package app.raspberryjam.cli.jvm.command
 
+import app.raspberryjam.cli.command.Monitor
 import com.github.ajalt.clikt.parameters.arguments.argument
 import com.github.ajalt.clikt.parameters.arguments.convert
 import com.github.ajalt.clikt.parameters.types.path
